eec673f7228954a6d55a12c1a0fda31d89cf7ae4
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2022-08-16 13:16:36 +0600

    array

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-let name = 'Mosh';
-console.log(name); 
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+person.name= 'John';
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 1;
+console.log(selectedColors);
 
 
-let firstName = 'Mosh'; 
-let lastName='Hamedani';
-
-let interestRate = 0.3;
-interestRate=1;
-console.log(interestRate);
-
-let name2 = 'Mosh2';
-let age = 30;
-let isApproved =false;
-let firstName = undefined;
-let selectColor = null; 
-
-
-
-
-
-
-
-
+let selection = 'name';
+person[selection] = 'Mary'; 
+console.log(person.name); 
