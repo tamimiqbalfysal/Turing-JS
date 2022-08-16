5eba8dd35a3737dedb78410603fc86bae4a5a07f
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2022-08-16 13:04:13 +0600

    variables

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+let name = 'Mosh';
+console.log(name); 
+
+
+let firstName = 'Mosh'; 
+let lastName='Hamedani';
+
+let interestRate = 0.3;
+interestRate=1;
+console.log(interestRate);
+
+let name2 = 'Mosh2';
+let age = 30;
+let isApproved =false;
+let firstName = undefined;
+let selectColor = null; 
+
+
+
+
+
+
+
+
