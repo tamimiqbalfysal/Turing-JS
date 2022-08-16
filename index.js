2ab38b6f1a56b0f7aeaf2b4056901d0abc1c8844
tamimiqbalfysal <tamimiqbal.fysal@gmail.com>
2022-08-16 19:18:45 +0600

    fizzbuzz

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-console.log(isLandscape(800,600)); 
+
+const output = fizzBuzz(5);
+console.log(output);
 
 
-function isLandscape(width,height){
-    return (width>height);
-    }
+function fizzBuzz(input){
+    if (typeof input !== 'number')
+    return 'Not a number';
 
+    if (input%3 ===0)
+    return 'Fizz';
+
+    if (input%5 ===0)
+    return 'Buzz';
+
+    if ((input%3 ===0) && (input %5 ===0))
+    return 'FizzBuzz';
+
+    return input; 
+}
 
